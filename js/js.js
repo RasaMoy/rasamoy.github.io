8642912397e73cce071da05972d6c43401379519
Rafael Rendon Solache <rasamoydas@gmail.com>
2017-09-14 14:24:29 -0500

    jsonp

--- js/js.js
+++ js/js.js
@@ -1,17 +1,34 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = function(){
-	if (xhr.status === 200) {
-		responseObject = JSON.parse(xhr.responseText);
 
-		var newContent = '';
-		for (var i = 0; i<responseObject.events.length; i++){
-			newContent += '<div class="event">';
-	        newContent += '<img src="' +responseObject.events[i].map +'" alt="">';
-	        newContent += '<p class="bold">' + responseObject.events[i].location +'</span></p></div>'
-		}
-		document.getElementById('content').innerHTML = newContent;
+//  --------------JSON
+
+// var xhr = new XMLHttpRequest();
+// xhr.onload = function(){
+// 	if (xhr.status === 200) {
+// 		responseObject = JSON.parse(xhr.responseText);
+
+// 		var newContent = '';
+// 		for (var i = 0; i<responseObject.events.length; i++){
+// 			newContent += '<div class="event">';
+// 	        newContent += '<img src="' +responseObject.events[i].map +'" alt="">';
+// 	        newContent += '<p class="bold">' + responseObject.events[i].location +'</span></p></div>'
+// 		}
+// 		document.getElementById('content').innerHTML = newContent;
+// 	}
+// };
+
+// xhr.open('GET', '../data/data.json', true);
+// xhr.send(null);
+
+
+//  --------------JSONP
+
+function showEvents(data){
+	var newContent = '';
+
+	for(var i = 0; i < data.events.length; i++){
+		newContent += '<div class="event">';
+	    newContent += '<img src="' + data.events[i].map +'" alt="">';
+	    newContent += '<p class="bold">' + data.events[i].location +'</span></p></div>'
 	}
-};
-
-xhr.open('GET', '../data/data.json', true);
-xhr.send(null);
+	document.getElementById('content').innerHTML = newContent;
+}
