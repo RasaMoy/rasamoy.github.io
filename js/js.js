36f4b5b8f91d3b970f6e794429246ad3cb2038d6
Rafael Rendon Solache <rasamoydas@gmail.com>
2017-09-15 21:44:40 -0500

    j

--- js/js.js
+++ js/js.js
@@ -43,7 +43,7 @@ $('nav a').on('click', function(e){
 	$(this).addClass('active');
 
 	$('main').remove();
-	$('.container').load(url + '#container');
+	$('.container').load(url + '#main');
 
 });
 
