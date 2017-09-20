260c160625850e93939cb4bc55b6b70c31fef7c8
Rafael Rendon Solache <rasamoydas@gmail.com>
2017-09-19 21:21:03 -0500

    menu terminado

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,23 @@
 var $buttonMenu = $('#button-menu');
+// -------------------------
+// -----MOSTRANDO MENU------
 
 function abrirMenu(){
 	$buttonMenu.removeClass('fa fa-bars').addClass('fa fa-close');
 	$('.navegacion .menu').css({'left':'0px'});
-	$('#site-cache').css({'position':'absolute'});
+	$('#site-cache').fadeIn().addClass('site-cache');
+	$('div.main').css({'left':'320px'});
 };
 
 function cerrarMenu(){
 	$buttonMenu.removeClass('fa fa-close').addClass('fa fa-bars');
 	$('.navegacion .menu').css({'left':'-320px'});
+	$('.navegacion .submenu').css({'left':'-320px'});
+	
 	$('#site-cache').fadeOut( 200, function() {
-		$('#site-cache').css({'position':'static'})
+		$('#site-cache').removeClass('site-cache');
 	});
+	$('div.main').css({'left':'0px'});
 
 }
 
@@ -21,4 +27,28 @@ $buttonMenu.click(function() {
 	}else{
 		cerrarMenu();
 	}
+});
+
+// -------------------------
+// -----MOSTRANDO SUBMENU------
+
+// MOSTRANDO SUBMENU
+	$('.navegacion .menu > .item-submenu a').click(function(){
+		
+		var positionMenu = $(this).parent().attr('menu'); // Buscamos el valor del atributo menu y lo guardamos en una variable
+		console.log(positionMenu); 
+
+		$('.item-submenu[menu='+positionMenu+'] .submenu').css({'left':'0px'}); // Mostramos El submenu correspondiente
+
+	});
+
+	// OCULTANDO SUBMENU
+	$('.navegacion .submenu li.atras').click(function(){
+
+		$(this).parent().css({'left':'-320px'}); // Ocultamos el submenu
+
+	});
+
+$('#site-cache').click(function(){
+	cerrarMenu();
 });
